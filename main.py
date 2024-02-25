104158b49e5300f766e15d36fa992e96c15e2815
David Veytsman <david.veytsman@gmail.com>
2024-02-25 12:03:52 -0500

    - Initial Commit - added game play functionality - added art - added game data

---
+++ main.py
@@ -0,0 +1,88 @@
+from art import vs, logo
+from game_data import data
+from random import choice
+# Preliminary Game Design
+# 1. print logo
+# 2. if this is the first turn generate 2 dicts from the data
+# 3. print first dict
+# 4. print the vs art
+# 5. print the second dict
+# 6. compare and have the higher ready
+# 7. ask user for input, if it doesn't match, game over
+# 8. if it matches round count increases
+# 9. prints winner of last round 
+# 10. Prints vs
+# 11. prints new dict
+# grab winner 
+# 13. ask user for input (this loops step 5 -> 11)
+print(logo)
+testdata = data
+guesses = 0
+
+def get_choice(testdata):
+    '''A function that takes the current state of the data and returns a random user from the current data'''
+
+    return choice(testdata)
+
+def remove_option(current_data, choice):
+    '''A function that takes in the current list of users and a user to remove, removes the user from the list and returns the modified list'''
+
+    current_data.remove(choice)
+    return current_data
+
+def get_winner(choicea, choiceb):
+    '''takes in 2 people and returns the person with the highest 
+    follower count'''
+    counta = get_follower_count(choicea)
+    countb = get_follower_count(choiceb)
+
+    if counta > countb:
+        return choicea
+    elif countb > counta:
+        return choiceb
+    
+def format_choice(choice):
+    '''Takes in a user and formats the output string'''
+
+    name = choice['name']
+    followers = choice['follower_count']
+    description = choice['description']
+    country = choice['country']
+    output = f"{name}, a {description}, from {country}"
+    return output
+
+def get_follower_count(user):
+    '''A function that takes in a user and returns the follower 
+       count as an integer'''
+    
+    return user['follower_count']
+
+winner = get_choice(data)
+play = True
+while play:
+    if guesses == 0:
+        choice_a = get_choice(testdata)
+        testdata = remove_option(testdata, choice_a)
+    else:
+        choice_a = winner
+    choice_b = get_choice(testdata)
+    testdata = remove_option(testdata, choice_b)
+
+    first = f"Compare A: {format_choice(choice_a)}"
+    second = f"Against B: {format_choice(choice_b)}"
+
+    winner = get_winner(choice_a, choice_b)
+    print(first)
+
+    print(vs)
+
+    print(second)
+    
+    guess = input("Who has more followers? A or B: ").upper()
+    if guess == "A" and winner['name'] == choice_a['name']:
+        guesses += 1
+    elif guess == "B" and winner['name'] == choice_b['name']:
+        guesses += 1
+    else:
+        play = False
+        print(f"you've lost with {guesses} number of correct guesses")
